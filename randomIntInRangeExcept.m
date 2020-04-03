dcb444c3d038f3e3399756e7db9c39a18c7704bb
BenOverzat <bjo12@miami.edu>
2020-04-03 19:46:48 -0400

    Delete randomIntInRangeExcept.m

--- randomIntInRangeExcept.m
+++
@@ -1,9 +0,0 @@
-function r = randomIntInRangeExcept(a,b,except,varargin)
-
-% Returns random integers in range [a,b] (inclusive), except for numbers in
-% "except"
-
-r = randomIntInRange(a,b-1,varargin{:});
-r(r>=except) = r(r>=except)+1;
-
-end
