0674484f997667931313047ec8b23523e07ee328
BenOverzat <bjo12@miami.edu>
2020-04-03 19:46:25 -0400

    Delete randomIntInRange.m

--- randomIntInRange.m
+++
@@ -1,8 +0,0 @@
-function r = randomIntInRange(a,b,varargin)
-
-% Returns a random integer in range [a,b] (inclusive)
-% Additional arguments indicate size of returned matrix
-
-r = a-1+randi(b-a+1,varargin{:});
-
-end
